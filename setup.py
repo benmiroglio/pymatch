b1b2cc418674c1cad5c2a1c9cf65d5680fb065e9
benmiroglio <bmiroglio@mozilla.com>
2017-09-20 14:10:24 -0700

    Add source to setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'matcher',
+  packages = ['matcher'],
+  version = '0.1',
+  description = 'Matching techniques for Observational Studies',
+  author = 'Ben Miroglio',
+  author_email = 'benmiroglio@gmail.com',
+  url = 'https://github.com/benmiroglio/matcher', 
+  download_url = 'https://github.com/benmiroglio/matcher/archive/0.1.tar.gz', 
+  keywords = ['logistic', 'regression', 'matching', 'observational', 'study', 'causal', 'inference'],
+  classifiers = [],
+)
